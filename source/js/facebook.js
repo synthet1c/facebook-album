72484624987ac948eccc51bcafab8c84f4640026
synthet1c <founts24@gmail.com>
2017-02-10 01:45:00 +1100

    added profile to album

--- source/js/facebook.js
+++ source/js/facebook.js
@@ -1,6 +1,6 @@
 import Task from 'data.task'
 import element, { div, section, h3, p, ul, li, img, a, span, svg } from './template'
-import { compose, curry, prop, head, last, map, chain, take, trace, lift } from './utils'
+import { compose, curry, prop, head, last, map, chain, take, trace, liftA3 } from './utils'
 
 import '../scss/facebook.scss'
 
@@ -23,7 +23,8 @@ export const facebook = (cb) => {
     FB.init({
       appId: APP_ID,
       xfbml: true,
-      version: 'v2.3'
+      version: 'v2.3',
+      access_token: ACCESS_TOKEN
     })
     cb(FB)
   }
@@ -31,9 +32,20 @@ export const facebook = (cb) => {
   document.head.appendChild(script)
 }
 
+export const getProfile = profileName => new Task((reject, resolve) => {
+  FB.api(`/${profileName}`, {
+    access_token: ACCESS_TOKEN
+  } ,resolve)
+})
+
+export const getProfileAlbums = profileName => new Task((reject, resolve) => {
+  FB.api(`/${profileName}/albums`, {
+    access_token: ACCESS_TOKEN
+  } ,resolve)
+})
+
 export const getPhotos = albumId => new Task((reject, resolve) => {
   FB.api(`/${albumId}/photos`, {
-    fields: photoFields,
     access_token: ACCESS_TOKEN
   } ,resolve)
 })
@@ -44,6 +56,18 @@ export const getAlbum = albumId => new Task((reject, resolve) => {
   } ,resolve)
 })
 
+export const getPhoto = photoId => new Task((reject, resolve) => {
+  FB.api(`/${photoId}`, {
+    access_token: ACCESS_TOKEN
+  } ,resolve)
+})
+
+const getCoverPhoto = album => new Task((reject, resolve) => {
+  FB.api(`/${album.cover_photo}`, {
+    access_token: ACCESS_TOKEN
+  }, cover => resolve({...album, cover}))
+})
+
 const header = heading =>
   div('.fb-album__header',
     h3('fb-album__heading', heading)
@@ -51,12 +75,32 @@ const header = heading =>
 
 const getImage = compose(prop('source'), last, prop('images'))
 
+const loadImages = images => Promise.all(
+  images.map(image => 
+    new Promise((resolve, reject) => {
+      const img = new Image
+      img.onload = () => resolve(img)
+      img.onerror = reject
+      img.src = image.source
+    })
+  )
+)
+
+const loadImagesTest = images => new Promise(res => setTimeout(res, 600))
+
+
 const icon = () => 
-  svg('svg', '#fb_icon.fb-album__icon', { 'viewBox': '0 0 1024 1024', 'width': '100%' },
+  svg('svg', '#fb_icon.fb-album__icon', { viewBox: '0 0 1024 1024', width: '100%' },
     svg('g', '.fb-album__icon--g', 
       svg('path', '.fb-album__icon--path', {
-        'fill': '#ffffff',
-        'd': `M621.273,512.188h-71.75V768H443.211V512.188h-50.562v-90.375h50.562v-58.5   C443.211,321.5,463.086,256,550.492,256l78.75,0.312v87.75h-57.156c-9.312,0-22.562,4.656-22.562,24.594v53.156h81.062   L621.273,512.188z M863.586,0h-704c-88.375,0-160,71.688-160,160v704c0,88.375,71.625,160,160,160h704c88.375,0,160-71.625,160-160   V160C1023.586,71.688,951.961,0,863.586,0`
+        fill: '#ffffff',
+        d: [
+          `M621.273,512.188h-71.75V768H443.211V512.188h-50.562v-90.375h50.562v-58.5`,
+          `C443.211,321.5,463.086,256,550.492,256l78.75,0.312v87.75h-57.156c-9.312,0-22.562,4.656-22.562,24.594v53.156h81.062`,
+          `L621.273,512.188z`,
+          `M863.586,0h-704c-88.375,0-160,71.688-160,160v704c0,88.375,71.625,160,160,160h704c88.375,0,160-71.625,160-160`,
+          `V160C1023.586,71.688,951.961,0,863.586,0`
+        ].join(' ')
       })
     )
   )
@@ -64,19 +108,24 @@ const icon = () =>
 // {Image} -> {Image}
 const attachHTML = album => ({
   ...album,
-  html: section('.fb-album',
+  html: section('.fb-album.loading',
     div('.fb-album__inner',
       header(album.name),
-      div('fb-album__list',
+      div('.fb-album__list',
         ...album.photos.data.map(photo =>
           div('.fb-album__item',
             div('.fb-album__img', {
               style: {
-                backgroundImage: `url(${photo.source})`
+                backgroundImage: 'url(' + photo.source + ')'
               }
             }),
             div('.fb-album__cover',
-              a('.fb-album__link', { href: photo.link }, icon())
+              a('.fb-album__link', { href: photo.link }, 
+                div('.fb-album__text',
+                  icon(),
+                  // p('.fb-album__location', photo.place.location.city)
+                )
+              )
             )
           )
         )
@@ -86,22 +135,31 @@ const attachHTML = album => ({
 })
 
 
+  
+
 facebook(function(FB) {
 
-  const albumAndImages = curry((album, photos) => ({
+  const albumAndImages = curry((profile, album, photos) => ({
     ...album,
-    photos: { data: take(4, photos.data) }
+    profile,
+    photos: { data: take(6, photos.data) }
   }))
 
-  const lifted = lift(albumAndImages, getAlbum(ALBUM_ID), getPhotos(ALBUM_ID))
-    .map(trace('applied'))
+  const lifted = liftA3(albumAndImages, getProfile(PROFILE), getAlbum(ALBUM_ID), getPhotos(ALBUM_ID))
     .map(attachHTML)
+    .map(trace('applied'))
 
   lifted.fork(
     e => console.error(e),
     album => {
-      document.querySelector('#app').appendChild(album.html)
-      console.log({ album })
+      const app = document.querySelector('#app')
+      
+      app.appendChild(album.html)
+      
+      loadImages(album.photos.data)
+        .then(() => {
+          app.querySelector('.fb-album').classList.remove('loading')    
+        })
     }
   )
 
--- source/js/utils.js
+++ source/js/utils.js
@@ -21,5 +21,10 @@ export const map = curry((f, obj) => obj.map(f))
 export const chain = curry((f, obj) => obj.chain(f))
 export const take = curry((n, a) => a.slice(0, n))
 export const trace = name => x => (console.log(name, x), x)
-export const lift = (fn, o1, o2) => o1.map(fn).ap(o2)
+
+export const liftA2 = (fn, o1, o2) => o1.map(fn).ap(o2)
+export const liftA3 = (fn, o1, o2, o3) => o1.map(fn).ap(o2).ap(o3)
+export const liftA4 = (fn, o1, o2, o3, o4) => o1.map(fn).ap(o2).ap(o3).ap(o4)
+export const liftA5 = (fn, o1, o2, o3, o4, o5) => o1.map(fn).ap(o2).ap(o3).ap(o4).ap(o5)
+export const liftA6 = (fn, o1, o2, o3, o4, o5, o6) => o1.map(fn).ap(o2).ap(o3).ap(o4).ap(o5).ap(o6)
 
